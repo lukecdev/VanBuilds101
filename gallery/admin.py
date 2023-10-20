ae7c134da549aa9f4d5d5e47e1ab973a5f7c0f83
lukecdev <courtney.luke@outlook.com>
2023-10-20 19:46:29 +0000

    added templates updates

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Image
+from .models import Image , Imagesubmit
 
 class ImageAdmin(admin.ModelAdmin):
     list_display = ['title', 'image']
 
-admin.site.register(Image, ImageAdmin)
+@admin.site.register(Image, ImageAdmin)
+
+
+@admin.register(Imagesubmit)
+class ImagesubmitAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_images']
+
+    def approve_image(self, request, queryset):
+        queryset.update(approved=True)
