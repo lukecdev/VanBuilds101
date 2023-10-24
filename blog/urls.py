f752772b13a499ed7fd1246464dedf0508a8f568
lukecdev <courtney.luke@outlook.com>
2023-10-24 15:33:31 +0000

    added crud update post for admin

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ from .views import UpdatePostView
 urlpatterns = [
     path('', views.PostList.as_view(), name="home"),
     path('create_post/', PostCreateView.as_view(), name='create_post'),
-    path('update_post/<int:pk>', UpdatePostView.as_view(), name='update_post'),
+    path('update_post/<slug:slug>', UpdatePostView.as_view(), name='update_post'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     # path('post/', PostCreateView.as_view(), name='add_post'),
